b99c38f6f507535640f36a126b8e1d83d0dd8d57
Anson Lee <a.lee3213@hotmail.com>
2020-03-08 17:54:22 +1100

    Fix bug where css files were not being sent

--- http_responses.c
+++ http_responses.c
@@ -133,8 +133,10 @@ void print_content_type(int socket_fd, char *filename) {
                                                   "Content-Type: text/plain\n");
         write(socket_fd, buffer, strlen(buffer));
     } else if (strstr(filename, ".css")) {
+        printf("filename: %s, its a css\n", filename);
         snprintf(buffer, MAX_HEADER_RESPONSE_LEN, "HTTP/1.1 200 OK\n"
                                                   "Content-Type: text/css\n");
+        write(socket_fd, buffer, strlen(buffer));
     } else if (strstr(filename, ".jpg") || strstr(filename, ".jpeg")) {
         snprintf(buffer, MAX_HEADER_RESPONSE_LEN, "HTTP/1.1 200 OK\n"
                                                   "Content-Type: image/jpeg\n");
